49d117d57737609c365198daf6aecd2b65f38e1c
jherryPonce <jherry2412@gmail.com>
2022-10-06 00:40:42 -0500

    darkmode

--- script.js
+++ script.js
@@ -17,4 +17,13 @@ buttons.forEach(item => {
     }
 })
 
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
 
+
+themeToggleBtn.onclick = () => {
+    //classList.toggle("nombre de la calse")de esta manera se activay desactiva de manera alternada la clase que se indica
+  calculator.classList.toggle("dark"); 
+  themeToggleBtn.classList.toggle("active");
+ 
+};
